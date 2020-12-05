090bd189bb2d07ea50d11fdccbf304f9a7b3a968
Luccas Santos <luccasjsantos@hotmail.com>
2020-12-05 00:30:30 -0300

    :ambulance: Fix an issue where 'path' keeps previous processed data
    This fix disables path's ability to map over arrays

--- src/path.js
+++ src/path.js
@@ -1,29 +1,7 @@
-const curry = require('./curry')
-const cond = require('./cond')
-const compose = require('./compose')
 const prop = require('./prop')
-const isObject = require('./isObject')
-const identity = require('./identity')
-const T = require('./T')
-const isArray = require('./isArray')
-const map = require('./map')
-const flat = require('./flat')
-const isUndef = require('./isUndef')
-const undef = require('./undef')
-const bind = require('./bind')
-const when = require('./when')
+const reduce = require('./reduce')
+const flip = require('./flip')
 
-const path = curry(
-  ([p, ...ps], args) => cond([
-    [bind(p, isUndef), identity],
-    [isObject, compose(path(ps), prop(p))],
-    [isArray, map(path([p, ...ps]))],
-    [T, undef]
-  ], args)
-)
-
-module.exports = curry(
-  props => compose(
-    when(isArray, flat),
-    path(props))
+module.exports = flip(
+  reduce(flip(prop))
 )
