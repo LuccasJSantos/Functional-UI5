2a979093f79bfe38bc5452ce7997160bdc42bf34
Luccas Santos <luccasjsantos@hotmail.com>
2020-12-17 23:00:43 -0300

    :loud_sound: Edit message when trying to get an inexistent model

--- src/ui5/getModel.js
+++ src/ui5/getModel.js
@@ -11,7 +11,7 @@ module.exports = curry(
     const model = parent.getModel(name)
 
     if (!model) {
-      const error = new Error(`Provided model '${name}' doesn't exist`)
+      const error = new Error(`Provided model '${name}' doesn't exist. If 'for' function is used, a new model will be created.`)
       console.info(error)
       return undefined
     }
