9b97dbea2a1b912cf8f2c79d162d52613a4c18d4
LuccasJSantos <luccasjsantos@hotmail.com>
2021-08-24 21:45:01 -0300

    :boom: fix flatMap() behaviour, it wasn't working as it should be
    also add test to it

--- src/flatMap.js
+++ src/flatMap.js
@@ -1,21 +1,6 @@
-const isArray = require('./isArray')
 const curry = require('./curry')
 const flat = require('./flat')
 
-const result = []
-
-const flatMap = curry(
-  (fn, arr) => {
-    if (arr.length === 0) return arr
-
-    arr.forEach(item => {
-      if (!isArray(item)) return result.push(fn(item))
-
-      result.push(...flat(item))
-    })
-
-    return result
-  }
+module.exports = curry(
+  (fn, arr) => flat(arr).map(fn)
 )
-
-module.exports = flatMap
