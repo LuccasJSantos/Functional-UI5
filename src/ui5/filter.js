b556bb4f8eca57a35962dfef73be90ab705fab8f
LuccasJSantos <luccasjsantos@hotmail.com>
2022-12-26 14:41:00 -0300

    make ui5 filter and map functions use the library functions

--- src/ui5/filter.js
+++ src/ui5/filter.js
@@ -1,9 +1,10 @@
 const curry = require('../curry')
+const filter = require('../filter')
 const getData = require('./getData')
 
 module.exports = curry(
   (parent, name, fn) => {
-    const data = getData(parent, name).filter(fn)
+    const data = filter(fn, getData(parent, name))
 
     return data
   })
--- src/ui5/map.js
+++ src/ui5/map.js
@@ -1,9 +1,10 @@
 const curry = require('../curry')
+const map = require('../map')
 const getData = require('./getData')
 
 module.exports = curry(
   (parent, name, fn) => {
-    const data = getData(parent, name).map(fn)
+    const data = map(fn, getData(parent, name))
 
     return data
   })
